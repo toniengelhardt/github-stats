ee5281ab5f11f46d36ceae9ebd695be07439d98d
Toni Engelhardt <toni88x@gmail.com>
2022-03-10 15:02:30 +0000

    Redirect logic ready

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const express = require('express')
 const path = require('path')
 
+// import Plausible from 'plausible-tracker'
+
+// const { trackPageview } = Plausible({
+//   trackLocalhost: true,
+// })
+
 const app = express()
 const port = process.env.PORT || 8080
 
+function redirectRoute(req, res, next) {
+  // trackPageview({
+  //   url: req.originalUrl,
+  // })
+  res.redirect(`https://repo-tracker.com/r/gh${req.url}`)
+}
+
 // Landing page
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, '/index.html'))
 })
+s
+// Redirect
+app.get("/*", redirectRoute)
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
