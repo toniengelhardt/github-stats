08f0cbc632c38868be3b65c2eba4c281756d5a02
Toni Engelhardt <toni88x@gmail.com>
2022-03-10 13:52:17 +0000

    Landing page

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = process.env.PORT || 8080
+
+// Landing page
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
