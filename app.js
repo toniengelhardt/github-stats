1cd534d3fca2a7e74d319e1ceba8bac9c4b62600
Toni Engelhardt <toni88x@gmail.com>
2022-03-10 15:47:43 +0000

    Add Plausible tracking

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const express = require('express')
 const path = require('path')
-
-// import Plausible from 'plausible-tracker'
-
-// const { trackPageview } = Plausible({
-//   trackLocalhost: true,
-// })
+const request = require('request')
 
 const app = express()
 const port = process.env.PORT || 8080
 
 function redirectRoute(req, res, next) {
-  // trackPageview({
-  //   url: req.originalUrl,
-  // })
+  // Send event to Plausible
+  if (!['localhost', '127.0.0.1'].includes(request.hostname)) {
+    request.post({
+      url:'https://plausible.io/api/event',
+      json: {
+        name: 'pageview',
+        domain: 'github-stats.com',
+        url: `https://github-stats.com${req.url}`,
+        // screen_width: ...
+      },
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Forwarded-For': request.ip,
+      }
+   })
+  }
+  // Redirect
   res.redirect(`https://repo-tracker.com/r/gh${req.url}`)
 }
 
@@ -21,8 +30,7 @@ function redirectRoute(req, res, next) {
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'))
 })
-s
-// Redirect
+// Redirects
 app.get("/*", redirectRoute)
 
 app.listen(port, () => {
